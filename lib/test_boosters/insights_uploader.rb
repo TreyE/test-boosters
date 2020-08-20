bb2803f1d025a68ba6c95244f4024b02ecd19440
Trey Evans <tevans@justplainawful.lan>
2020-08-20 09:19:00 -0400

    Make uploading opt-in.

--- lib/test_boosters/insights_uploader.rb
+++ lib/test_boosters/insights_uploader.rb
@@ -3,7 +3,7 @@ module TestBoosters
     module_function
 
     def upload(booster_type, file)
-      return unless File.exist?(file)
+      return unless ENV['TB_UPLOAD_INSIGHTS']
 
       cmd = "http POST '#{insights_url}' #{booster_type}:=@#{file}"
 
